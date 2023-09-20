b4da9a4a4ebacececa56029b1eff53892f3d0c0c
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-09-20 22:46:17 +0500

    Task0

--- Tyuiu.DubrovinSN.Sprint1.Task0.V7.Lib/DataService.cs
+++ Tyuiu.DubrovinSN.Sprint1.Task0.V7.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.DubrovinSN.Sprint1.Task0.V7.Lib
     public class DataService : ISprint1Task0V7
     {
         public double Calculate()
-        {   //
+        {   
             return (18 / 6*2 - (4 - 3));
         }
     }
