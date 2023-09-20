2fec34f28b8409052f23023754e9b0a06a3765e3
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-09-20 22:30:34 +0500

    Task2

---
+++ Tyuiu.DubrovinSN.Sprint1.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint1.Task1.V2.Lib;
+
+namespace Tyuiu.DubrovinSN.Sprint1.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Math()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 12.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
---
+++ Tyuiu.DubrovinSN.Sprint1.Task1.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DubrovinSN.Sprint1.Task1.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DubrovinSN.Sprint1.Task1.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fc08125-93e0-4317-a266-1e34b8bcaa30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
